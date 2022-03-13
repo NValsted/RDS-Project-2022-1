7cfb1d8300929db96f672de94649cfc9d06bae5a
NValsted <nicolaj.valsted@live.dk>
2022-03-13 23:58:52 +0100

    Add pandas profiling and ability to query latest log points

--- report.py
+++ report.py
@@ -1,15 +1,19 @@
 from io import StringIO
 
-from src.stats import generate_figures
+from src.stats import generate_figures, profile_report
 from src.mail import send_email
 
 
 def send_report():
+    pandas_profile = profile_report()
     distplot, scatter = generate_figures()
 
     logs = StringIO()
     with open("logs.log", "r") as f:
         logs.write(f.read())
+
+    pandas_profile_stream = StringIO()
+    pandas_profile_stream.write(pandas_profile.to_html())
 
     scatter_stream = StringIO()
     scatter_stream.write(scatter.to_html())
@@ -19,6 +23,7 @@ def send_report():
 
     send_email(
         attachments=[
+            ("pandas-profiling-report.html", pandas_profile_stream),
             ("scatter.html", scatter_stream),
             ("distplot.html", distplot_stream),
             ("logs.log", logs),
--- src/stats.py
+++ src/stats.py
@@ -4,6 +4,7 @@ from enum import Enum
 import pandas as pd
 import plotly.figure_factory as ff
 import plotly.express as px
+from pandas_profiling import ProfileReport
 
 from src.database import DBFactory
 from src.database_models import RedditPostTable, RedditPostLogPointTable
@@ -29,7 +30,7 @@ def get_scatter(data, attribute: TargetAttributeEnum = TargetAttributeEnum.SCORE
     return px.scatter(data, x="date", y=attribute.value, color="group", opacity=0.25)
 
 
-def generate_figures():
+def get_df(latest_log_point: bool = False):
     posts = pd.read_sql_table(RedditPostTable.__tablename__, DBFactory.engine_url)
     log_points = pd.read_sql_table(
         RedditPostLogPointTable.__tablename__, DBFactory.engine_url
@@ -38,7 +39,31 @@ def generate_figures():
     posts = posts[posts["creation_date"] >= (datetime.now() - timedelta(days=8))]
     joined = pd.merge(posts, log_points, on="id", how="left")
 
-    distplot = get_distplot(joined)
-    scatter = get_scatter(joined)
+    if latest_log_point:
+        sorted = joined.sort_values(["date"], ascending=False)
+        grouped = sorted.groupby(by="id")
+        latest = grouped.first()
+        return latest
+    else:
+        if len(joined) > 10000:
+            return joined[joined["score"] >= 10]
+        return joined
+
+
+def generate_figures():
+    df = get_df()
+    scatter = get_scatter(df)
+
+    df_latest_points = get_df(latest_log_point=True)
+    distplot = get_distplot(df_latest_points)
 
     return distplot, scatter
+
+
+def profile_report():
+    df = get_df(latest_log_point=True)
+
+    report = ProfileReport(
+        df, title="Reddit Post Report", explorative=True, minimal=True
+    )
+    return report
